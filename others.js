0734ae3ac0ccf85a33104035838aa1ac00422015
qq57240 <xiaopan@qq.com>
2022-04-07 13:22:14 +0800

    Add files via upload

---
+++ others.js
@@ -0,0 +1,518 @@
+"islands": {
+    "1": {
+        cn: "阿玛克纳"
+    },
+    "2": {
+        cn: "邦塔"
+    },
+    "3": {
+        cn: "布拉克玛"
+    },
+    "4": {
+        cn: "萨弗基亚"
+    },
+    "8": {
+        cn: "无主之地"
+    },
+    "13": {
+        cn: "冒险岛"
+    }
+},
+"jobs": {
+    "40": {
+        cn: "面点师"
+    },
+    "62": {
+        cn: ""
+    },
+    "64": {
+        cn: "种植"
+    },
+    "67": {
+        cn: "造币"
+    },
+    "68": {
+        cn: "近战武器大师"
+    },
+    "69": {
+        cn: "远程武器大师"
+    },
+    "70": {
+        cn: "魔法武器大师"
+    },
+    "71": {
+        cn: "伐木"
+    },
+    "72": {
+        cn: "草药"
+    },
+    "73": {
+        cn: "采矿"
+    },
+    "74": {
+        cn: "畜牧"
+    },
+    "75": {
+        cn: "钓鱼"
+    },
+    "76": {
+        cn: "厨师"
+    },
+    "77": {
+        cn: "制甲师"
+    },
+    "78": {
+        cn: "珠宝师"
+    },
+    "79": {
+        cn: "裁缝"
+    },
+    "80": {
+        cn: "皮匠"
+    },
+    "81": {
+        cn: "工匠"
+    },
+    "82": {
+        cn: "制造"
+    },
+    "83": {
+        cn: "武器大师"
+    }
+},
+"rarities": {
+    "0": {
+        cn: "劣质"
+    },
+    "1": {
+        cn: "普通"
+    },
+    "2": {
+        cn: "优质"
+    },
+    "3": {
+        cn: "神秘"
+    },
+    "4": {
+        cn: "传奇"
+    },
+    "5": {
+        cn: "遗物"
+    },
+    "6": {
+        cn: "PVP"
+    },
+    "7": {
+        cn: "史诗"
+    }
+},
+"origines": {
+    "ORIGIN_DROP": {
+        cn: "掉落"
+    },
+    "ORIGIN_CRAFT": {
+        cn: "制造"
+    },
+    "ORIGIN_TOKEN": {
+        cn: "牌子"
+    },
+    "ORIGIN_ACHIEVEMENT": {
+        cn: "成就"
+    },
+    "ORIGIN_SHOPS": {
+        cn: "商城"
+    }
+},
+"effects": {
+    "1": {
+        cn: "伤害",
+        en: "Damage"
+    },
+    "2": {
+        cn: "治疗",
+        en: "Heals"
+    },
+    "4": {
+        cn: "生命",
+        en: "Bonus max HP"
+    },
+    "5": {
+        cn: "治疗",
+        en: "Heal bonus"
+    },
+    "6": {
+        cn: "行动力",
+        en: "AP bonus"
+    },
+    "7": {
+        cn: "行动力损失抗性",
+        en: "AP loss resistance"
+    },
+    "8": {
+        cn: "移动力",
+        en: "MP boost"
+    },
+    "10": {
+        cn: "移动力损失抗性",
+        en: "MP loss resistance"
+    },
+    "12": {
+        cn: "单元素等级加成",
+        en: "Branch level bonus"
+    },
+    "13": {
+        cn: "全元素等级加成",
+        en: "level bonus to all spells"
+    },
+    "15": {
+        cn: "背刺抗性",
+        en: "Back damage resistance"
+    },
+    "16": {
+        cn: "全元素抗性",
+        en: "Resistance to all elements"
+    },
+    "17": {
+        cn: "火抗",
+        en: "Fire resistance"
+    },
+    "18": {
+        cn: "水抗",
+        en: "Water resistance"
+    },
+    "19": {
+        cn: "土抗",
+        en: "Earth resistance"
+    },
+    "20": {
+        cn: "风抗",
+        en: "Air resistance"
+    },
+    "21": {
+        cn: "伤害",
+        en: "Damage bonus (%)"
+    },
+    "22": {
+        cn: "火伤百分比",
+        en: "Fire damage bonus (%)"
+    },
+    "23": {
+        cn: "土伤百分比",
+        en: "Earth damage bonus (%)"
+    },
+    "24": {
+        cn: "水伤百分比",
+        en: "Water damage bonus (%)"
+    },
+    "25": {
+        cn: "风伤百分比",
+        en: "Air damage bonus (%)"
+    },
+    "26": {
+        cn: "暴击伤害",
+        en: "Critical Hits damage bonus"
+    },
+    "27": {
+        cn: "暴击",
+        en: "Critical Hits bonus"
+    },
+    "30": {
+        cn: "韧性",
+        en: "Strength bonus"
+    },
+    "33": {
+        cn: "射程",
+        en: "Range bonus"
+    },
+    "34": {
+        cn: "勘探",
+        en: "Prospecting bonus"
+    },
+    "35": {
+        cn: "悟性",
+        en: "Wisdom bonus"
+    },
+    "36": {
+        cn: "先手",
+        en: "Initiative bonus"
+    },
+    "37": {
+        cn: "锁定",
+        en: "Lock bonus"
+    },
+    "38": {
+        cn: "闪避",
+        en: "Dodge bonus"
+    },
+    "39": {
+        cn: "背刺",
+        en: "Back damage bonus"
+    },
+    "42": {
+        cn: "统率",
+        en: "Leadership bonus"
+    },
+    "43": {
+        cn: "装备熟练",
+        en: "Kit Skill bonus"
+    },
+    "44": {
+        cn: "造成移动力损失",
+        en: "Bonus to MP loss chances"
+    },
+    "45": {
+        cn: "熟练",
+        en: "Kit Skill bonus"
+    },
+    "49": {
+        cn: "召唤物伤害",
+        en: "Summons damage bonus"
+    },
+    "51": {
+        cn: "意志",
+        en: "Willpower bonus"
+    },
+    "52": {
+        cn: "格挡",
+        en: "Block bonus"
+    }
+},
+"bonus": {
+    "1": {
+        cn: "最大行动力",
+        en: "MAX AP"
+    },
+    "2": {
+        cn: "最大移动力",
+        en: "MAX MP"
+    },
+    "3": {
+        cn: "最大创生力",
+        en: "MAX WP"
+    },
+    "4": {
+        cn: "最大生命",
+        en: "MAX HP"
+    },
+    "5": {
+        cn: "行动力",
+        en: "AP"
+    },
+    "6": {
+        cn: "移动力",
+        en: "MP"
+    },
+    "7": {
+        cn: "创生力",
+        en: "WP"
+    },
+    "8": {
+        cn: "生命",
+        en: "HP"
+    },
+    "9": {
+        cn: "武器伤害",
+        en: "Damage"
+    },
+    "10": {
+        cn: "统率",
+        en: "Control"
+    },
+    "11": {
+        cn: "射程",
+        en: "Range"
+    },
+    "12": {
+        cn: "先手",
+        en: "Initiative"
+    },
+    "13": {
+        cn: "格挡",
+        en: "Block"
+    },
+    "14": {
+        cn: "闪避",
+        en: "Dodge"
+    },
+    "15": {
+        cn: "锁定",
+        en: "Lock"
+    },
+    "16": {
+        cn: "伤害",
+        en: "Mastery of"
+    },
+    "17": {
+        cn: "熟练",
+        en: "Kit Skill"
+    },
+    "18": {
+        cn: "暴击",
+        en: "Critical Hit"
+    },
+    "19": {
+        cn: "暴击伤害",
+        en: "Critical Mastery"
+    },
+    "20": {
+        cn: "单体伤害",
+        en: "Single Target Mastery"
+    },
+    "21": {
+        cn: "范围伤害",
+        en: "Area Mastery"
+    },
+    "22": {
+        cn: "远程伤害",
+        en: "Distance Mastery"
+    },
+    "23": {
+        cn: "近战伤害",
+        en: "Melee Mastery"
+    },
+    "24": {
+        cn: "狂暴伤害",
+        en: "Berserk Mastery"
+    },
+    "25": {
+        cn: "背刺伤害",
+        en: "Rear Mastery"
+    },
+    "26": {
+        cn: "治疗",
+        en: "Healing Mastery"
+    },
+    "27": {
+        cn: "种随机元素",
+        en: "random elements"
+    },
+    "28": {
+        cn: "种随机元素",
+        en: "random element"
+    },
+    "29": {
+        cn: "元素抗性",
+        en: "Elemental Resistance"
+    },
+    "30": {
+        cn: "暴击抗性",
+        en: "Critical Resistance"
+    },
+    "31": {
+        cn: "单体抗性",
+        en: "Single Target Resistance"
+    },
+    "32": {
+        cn: "范围抗性",
+        en: "Area Resistance"
+    },
+    "33": {
+        cn: "远程抗性",
+        en: "Distance Resistance"
+    },
+    "34": {
+        cn: "近战抗性",
+        en: "Melee Resistance"
+    },
+    "35": {
+        cn: "狂暴抗性",
+        en: "Berserk Resistance"
+    },
+    "36": {
+        cn: "背刺抗性",
+        en: "Rear Resistance"
+    },
+    "37": {
+        cn: "火抗",
+        en: "Resistance Fire"
+    },
+    "38": {
+        cn: "水抗",
+        en: "Resistance Water"
+    },
+    "39": {
+        cn: "风抗",
+        en: "Resistance Air"
+    },
+    "40": {
+        cn: "土抗",
+        en: "Resistance Earth"
+    },
+    "41": {
+        cn: "抗性",
+        en: "Resistance to"
+    },
+    "42": {
+        cn: "护甲获得",
+        en: "Armor received"
+    },
+    "43": {
+        cn: "护甲给予",
+        en: "Armor given"
+    },
+    "44": {
+        cn: "移动速度",
+        en: "movement speed"
+    },
+    "45": {
+        cn: "伤害",
+        en: "Elemental Mastery"
+    },
+    "46": {
+        cn: "火伤",
+        en: "Mastery Fire"
+    },
+    "47": {
+        cn: "水伤",
+        en: "Mastery Water"
+    },
+    "48": {
+        cn: "风伤",
+        en: "Mastery Air"
+    },
+    "49": {
+        cn: "土伤",
+        en: "Mastery Earth"
+    },
+    "50": {
+        cn: "意志",
+        en: "Force of Will"
+    },
+    "51": {
+        cn: "耕种时的收获数量",
+        en: "Harvesting Quantity in Farmer"
+    },
+    "52": {
+        cn: "砍伐时的收获数量",
+        en: "Harvesting Quantity in Lumberjack"
+    },
+    "53": {
+        cn: "采药时的收获数量",
+        en: "Harvesting Quantity in Herbalist"
+    },
+    "54": {
+        cn: "开采时的收获数量",
+        en: "Harvesting Quantity in Miner"
+    },
+    "55": {
+        cn: "畜牧时的收获数量",
+        en: "Harvesting Quantity in Trapper"
+    },
+    "56": {
+        cn: "垂钓时的收获数量",
+        en: "Harvesting Quantity in Fisherman"
+    },
+    "57": {
+        cn: "行动力",
+        en: "Action points"
+    },
+    "58": {
+        cn: "行动力",
+        en: "Action Points"
+    }
+},
+"catchables": {
+    "1": {
+        cn: "可捕捉"
+    }
+}
